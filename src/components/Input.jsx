8af41be783a47e8eb137826c4f295edf1168fd4c
Hugh Stapleton <hstapleton808@gmail.com>
2023-11-21 17:23:47 -0500

    working on fet to NREL API

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Input = (props) => {
-    const { handleClick } = props
+  const { handleClick } = props;
+  const [inputCity, setInputCity] = useState(''); // Initialize state for the input value
 
-    return(
-        <div>
-        <h3>Please Enter a Major City</h3>
-        <form> 
-          <label htmlFor="location">City: </label>
-          <input id="location-value" type='text'></input>
-          <input type='button' value="Search City" onClick={handleClick}></input>
-        </form>
-      </div>
-    )
+  const handleInputChange = (event) => {
+    setInputCity(event.target.value); // Update the state with the input value
+  };
 
-}
+  const handleButtonClick = () => {
+    handleClick(inputCity); // Pass the inputCity to the parent component when the button is clicked
+    setInputCity('')
+  };
+
+  return (
+    <div>
+      <h3>Please Enter a Major City</h3>
+      <form>
+        <label htmlFor="location">City: </label>
+        <input
+          id="location-value"
+          type="text"
+          value={inputCity}
+          onChange={handleInputChange}
+        />
+        <input type="button" value="Search City" onClick={handleButtonClick} />
+      </form>
+    </div>
+  );
+};
 
 export default Input;
--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -11,17 +11,24 @@ const MainContainer = () => {
         cityName: 'Denver',
         annual: 6.06,
         },])
-
-    const handleClick = (submitData) => {
+    
+    
+    const handleClick = async (inputCity) => {
         //takes the data, excutes a fetch requeset with the info
-        console.log('You clicked the button!')
-        // () => {fetch(appropriateURL)
-        // .then((data) => data.json())
-        // .then((data) =>{
-        //     const avgDNI = data.outputs.avg_dni.annual
-        //     updateCities({cityName: 'newCity', avgDNI})
-        // })
+        console.log('You clicked the button! Your inputCity is:', inputCity);
+        const url = `/api/solar/solar_resource/v1.json?api_key=${APIKey}&address=${inputCity}`;
+        try{
+            const response = await fetch(url);
+            const data = await response.json();
+            const avgDNI = data.outputs.avg_dni.annual
+            console.log(data)
+                // updateCities((prevCities) => [...prevCities, { cityName: inputCity, avgDNI }]))
+            }
+        catch(err) {
+            console.log('ErrorFetching data:', err)
+        };
         }
+
 
 
     return(
